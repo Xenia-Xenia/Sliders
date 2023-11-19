b7594d3f6d88c0e8f1a053c4513110b64564d07e
Xenia Zagareva <xeniazagareva@gmail.com>
2023-11-19 17:54:25 +0300

    Completed ed.

--- Sliders/ViewController.swift
+++ Sliders/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var rSlider: UISlider! {
+        didSet {
+            rSlider.tintColor = UIColor.red
+        }
+    }
+    @IBOutlet var gSlider: UISlider! {
+        didSet {
+            gSlider.tintColor = UIColor.green
+        }
+    }
+    @IBOutlet var bSlider: UISlider!
+    
+    @IBOutlet var rLabel: UILabel!
+    @IBOutlet var gLabel: UILabel!
+    @IBOutlet var bLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    func changeColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(rSlider.value), green: CGFloat(gSlider.value), blue: CGFloat(bSlider.value), alpha: 1)
+    }
+    
+    
+    @IBAction func rgbSliderChanged(_ sender: Any) {
+        changeColor()
+        rLabel.text = String(format: "%.3f", rSlider.value)
+        gLabel.text = String(format: "%.3f", gSlider.value)
+        bLabel.text = String(format: "%.3f", bSlider.value)
+    }
+    
 }
 
